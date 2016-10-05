ab74dea23900190f5477b7c8bb7f86d368b83cb0
Tom W Hartung <tomwhartung@gmail.com>
2016-10-05 07:23:12 -0600

    Updated Site/settings.py to do the "hello world" thing.

--- Site/Site/settings.py
+++ Site/Site/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'content',
 ]
 
 MIDDLEWARE = [
@@ -69,17 +70,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Site.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/dev/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+##
+## Commenting this out for now
+##
+## # Database
+## # https://docs.djangoproject.com/en/dev/ref/settings/#databases
+##
+## DATABASES = {
+##     'default': {
+##         'ENGINE': 'django.db.backends.sqlite3',
+##         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+##     }
+## }
+##
 
 # Password validation
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
