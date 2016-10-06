3b59d40a24f90a4c1a6c7fd5244e6f05725d0ba3
Tom W Hartung <tomwhartung@gmail.com>
2016-10-05 18:03:59 -0600

    Removed the old "hello world" index function and added a new one that renders the index.html template file.

--- Site/content/views.py
+++ Site/content/views.py
@@ -9,20 +9,13 @@ from django.views.generic.base import View
 from django.template import loader
 
 ##
-## From part 1 of the polls tutorial; the simplest view:
-## Reference: "Write your first view" section
-##    https://docs.djangoproject.com/en/1.10/intro/tutorial01/
-## Runs when we access:
-##    http://127.0.0.1:8000/index
-##
+# load and render the template with a context containing a single variable
+#
 def index(request):
-    return HttpResponse("Hello from the index function in content/views.py .")
-
-def index_future(request):
-    context_var = 'arbitrary greeting text in a context variable'
-    template = loader.get_template('content/index.html')
-    context = {
-        'context_var': context_var,
-    }
-    return HttpResponse(template.render(context, request))
+  context_var = 'arbitrary greeting text in a context variable'
+  template = loader.get_template('content/index.html')
+  context = {
+    'context_var': context_var,
+  }
+  return HttpResponse(template.render(context, request))
 
