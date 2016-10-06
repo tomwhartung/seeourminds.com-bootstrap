f65391008f23cfc27cfc6a08e11372e0a3d55a89
Tom W Hartung <tomwhartung@gmail.com>
2016-10-05 19:08:29 -0600

    Removed the index function and added home and quiz functions, each serving pages with very minimal content, for the two pages we want on our site right now.

--- Site/content/views.py
+++ Site/content/views.py
@@ -9,13 +9,33 @@ from django.views.generic.base import View
 from django.template import loader
 
 ##
-# load and render the template with a context containing a single variable
+# load and render the template with a context containing
+# heading and content specific to the home page
 #
-def index(request):
-  context_var = 'arbitrary greeting text in a context variable'
+def home(request):
+  context_heading = 'New!'
+  content_s1 = 'Welcome to the NEW SeeOurMinds.com!  '
+  content_s2 = 'Sorry to disappoint you, but we upgraded our server and there is nothing here right now!  '
+  content_s3 = 'We are busy learning and playing around with the latest technologies, so check back again soon!  '
+  context_content = content_s1 + content_s2 + content_s3
   template = loader.get_template('content/index.html')
   context = {
-    'context_var': context_var,
+    'context_heading': context_heading,
+    'context_content': context_content,
+  }
+  return HttpResponse(template.render(context, request))
+
+##
+# load and render the template with a context containing
+# heading and content specific to the quiz page
+#
+def quiz(request):
+  context_heading = 'The SeeOurMinds.com Quiz'
+  context_content = 'Welcome to the quiz page.  There is nothing here right now; please try again later.  '
+  template = loader.get_template('content/index.html')
+  context = {
+    'context_heading': context_heading,
+    'context_content': context_content,
   }
   return HttpResponse(template.render(context, request))
 
