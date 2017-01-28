33a595ec1933e7846c73361d1beb7fbbac1600ff
Tom W Hartung <tomwhartung@gmail.com>
2017-01-28 16:50:16 -0700

    Removed code that sets context variables from views.py , because quiz.html no longer uses it.

--- Site/content/views.py
+++ Site/content/views.py
@@ -26,15 +26,8 @@ def home(request):
 #
 def quiz(request):
   context_quiz_selected = 'selected'
-  context_heading = 'The SeeOurMinds.com Quiz'
-  context_leading_ptags = [
-    'Welcome to the quiz page.',
-    'We are working on this, so please try again later.',
-  ]
   template = loader.get_template('content/quiz.html')
   context = {
     'context_quiz_selected': context_quiz_selected,
-    'context_heading': context_heading,
-    'context_leading_ptags': context_leading_ptags,
   }
   return HttpResponse(template.render(context, request))
