e8341981dc15dc08bf35c7f9b22e2838d859c2d2
Tom W Hartung <tomwhartung@gmail.com>
2017-01-30 18:09:55 -0700

    Added a url pattern for the new galleries page to urls.py .

--- Site/content/urls.py
+++ Site/content/urls.py
@@ -8,6 +8,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^home$', views.home, name='home'),
+    url(r'^galleries$', views.galleries, name='galleries'),
     url(r'^quiz$', views.quiz, name='quiz'),
 ]
-
