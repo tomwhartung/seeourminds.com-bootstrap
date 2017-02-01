2b9c1f3b31cdba1b97d93bf64c0dfdce42dfe794
Tom W Hartung <tomwhartung@gmail.com>
2017-02-01 16:47:21 -0700

    Updated the url entry for our gallery page so we can actually get the page.

--- Site/content/urls.py
+++ Site/content/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^home$', views.home, name='home'),
     url(r'^galleries$', views.galleries, name='galleries'),
-    url(r'^gallery$', views.gallery, name='gallery'),
+    url(r'^gallery', views.gallery, name='gallery'),
     url(r'^quiz$', views.quiz, name='quiz'),
 ]
