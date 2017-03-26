552a90930337dac257f9a9721a37b9c8c3faa6fd
Tom W Hartung <tomwhartung@gmail.com>
2017-03-26 16:02:20 -0600

    Updated urls.py for PEP8 compliance.  Definitely on a roll here!

--- Site/content/urls.py
+++ Site/content/urls.py
@@ -1,6 +1,13 @@
-#
-# urls.py for our content app
-#
+""" urls.py for our content app
+
+Purpose: define the urls for this app
+Author: Tom W. Hartung
+Date: Winter, 2017.
+Copyright: (c) 2017 Tom W. Hartung, Groja.com, and JooMoo Websites LLC.
+Reference:
+  (none)
+"""
+
 from django.conf.urls import *
 
 from . import views
@@ -12,5 +19,5 @@ urlpatterns = [
     url(r'^gallery/(?P<gallery_name>\w+)/$', views.gallery, name='gallery'),
     url(r'^quiz$', views.quiz, name='quiz'),
     url(r'^google428ef5aab2bc0870.html$',
-      views.google_verification, name='google_verification'),
+        views.google_verification, name='google_verification'),
 ]
