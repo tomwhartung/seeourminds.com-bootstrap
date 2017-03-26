3127761a511ebbccac8615fd053a26a15d8d957c
Tom W Hartung <tomwhartung@gmail.com>
2017-03-26 15:57:15 -0600

    Updated models.py for PEP8 compliance.

--- Site/content/models.py
+++ Site/content/models.py
@@ -1,3 +1,13 @@
+""" Placeholder models module is currently empty.
+
+Purpose: placeholder for if and when we want to implement one or more models
+Author: (none)
+Date: Winter, 2017.
+Copyright: (c) 2017 Tom W. Hartung, Groja.com, and JooMoo Websites LLC.
+Reference:
+  (none)
+"""
+
 from django.db import models
 
 # Create your models here.
