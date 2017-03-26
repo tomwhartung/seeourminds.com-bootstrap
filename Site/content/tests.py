983c9aa3bbd17d14dffb52e04b3438761ae70845
Tom W Hartung <tomwhartung@gmail.com>
2017-03-26 15:59:09 -0600

    Updated tests.py for PEP8 compliance.

--- Site/content/tests.py
+++ Site/content/tests.py
@@ -1,3 +1,13 @@
+""" Placeholder tests module is currently empty.
+
+Purpose: placeholder for if and when we want to implement one or more tests
+Author: (none)
+Date: Winter, 2017.
+Copyright: (c) 2017 Tom W. Hartung, Groja.com, and JooMoo Websites LLC.
+Reference:
+  (none)
+"""
+
 from django.test import TestCase
 
 # Create your tests here.
