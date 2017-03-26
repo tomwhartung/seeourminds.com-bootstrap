735aeffa352903c6a4f86829899353c59f536936
Tom W Hartung <tomwhartung@gmail.com>
2017-03-26 15:48:14 -0600

    Updated admin.py for PEP8 compliance, first time's a charm!

--- Site/content/admin.py
+++ Site/content/admin.py
@@ -1,3 +1,13 @@
+""" Placeholder admin module is currently empty.
+
+Purpose: placeholder for if and when we want to implement an admin module
+Author: (none)
+Date: Winter, 2017.
+Copyright: (c) 2017 Tom W. Hartung, Groja.com, and JooMoo Websites LLC.
+Reference:
+  (none)
+"""
+
 from django.contrib import admin
 
 # Register your models here.
