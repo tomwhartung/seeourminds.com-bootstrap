e741474116cc0832c143cf9e3ef1623a91368d20
Tom W Hartung <tomwhartung@gmail.com>
2017-04-10 14:31:14 -0600

    Adding adsense.py , to define the advertisements we are displaying to support monetization.

---
+++ Site/content/adsense.py
@@ -0,0 +1,24 @@
+""" adsense.py define Google AdSense ads for our content app
+
+Purpose: define the advertisements we are displaying to support monetization
+Author: Tom W. Hartung
+Date: Winter, 2017.
+Copyright: (c) 2017 Tom W. Hartung, Groja.com, and JooMoo Websites LLC.
+Reference:
+  (none)
+"""
+
+adsense_ads = {
+    "top_aside_top_ad":
+        '<h4>Google AdSense markup for the "top_aside_top_ad" block.</h4>',
+    "top_aside_bottom_ad":
+        '<h4>Google AdSense markup for the "top_aside_bottom_ad" block.</h4>',
+    "above_middle_row_ad":
+        '<h4>Google AdSense markup for the "above_middle_row_ad" block.</h4>',
+    "middle_left_ad":
+        '<h4>Google AdSense markup for the "middle_left_ad" block.</h4>',
+    "middle_right_ad":
+        '<h4>Google AdSense markup for the "middle_right_ad" block.</h4>',
+    "below_middle_row_ad":
+        '<h4>Google AdSense markup for the "below_middle_row_ad" block.</h4>',
+}
