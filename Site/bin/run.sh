b5149301fc1e58947707776daeda80383f9ac9ba
Tom W Hartung <tomwhartung@gmail.com>
2017-04-12 12:59:40 -0600

    Renamed USE_AD_PLACEHOLDERS to RUNNING_LOCALLY , for flexibility that might prove to be useful some time in the future.

--- Site/bin/run.sh
+++ Site/bin/run.sh
@@ -3,6 +3,6 @@
 #  run.sh: tiny shell script to run the development server for this app
 #
 ## export DJANGO_DEBUG=1
-export USE_AD_PLACEHOLDERS=1
+export RUNNING_LOCALLY=1
 export PYTHONPATH="..:${PYTHONPATH}"
 python3 -m manage runserver
