1656f3aece7a371539b79982f9e9e84ce0c1a725
Tom W Hartung <tomwhartung@gmail.com>
2017-04-28 14:09:43 -0600

    Updating to the new version of content/models.py from the 23-som_postgresql_exp project in the always_learning_python repo.  We have really made a shit-ton of changes!

--- Site/content/models.py
+++ Site/content/models.py
@@ -1,6 +1,6 @@
-""" Contains the models for our app.
+""" Contains the non-database models for our app.
 
-Purpose: just because we aren't using a db doesn't mean we can't have models
+Purpose: contains the models for unsaved data and read-only data in json format
 Author: Tom W. Hartung
 Date: Winter, 2017.
 Copyright: (c) 2017 Tom W. Hartung, Groja.com, and JooMoo Websites LLC.
@@ -10,19 +10,20 @@ Reference:
 
 import json
 import os
-from django.db import models
+from django.contrib import messages
+
+from .database import Questionnaire
 
 DJANGO_DEBUG = os.environ.get('DJANGO_DEBUG')
-site_content_dir = os.path.abspath(os.path.dirname(__file__))
-QUIZ_FILE_DIR = site_content_dir + '/static/content/json/quiz/'
-QUIZ_FILE_NAME = 'seeourminds_quiz.json'
 
 
-class Score(models.Model):
+class Score:
 
-    """ Class to contain the score for the quiz """
+    """ Class to calculate, contain, and display the score for the quiz """
 
     def __init__(self):
+        self.score_is_complete = False
+        self.unanswered_question_count = -1
         self.e_score = 0
         self.i_score = 0
         self.n_score = 0
@@ -46,19 +47,68 @@ class Score(models.Model):
         self.j_pct = None
         self.p_pct = None
 
-    def tally_answer(self, answer_123_type, answer_selected_int, answer_weight_int):
+    def score_quiz(self, quiz_size_slug, cleaned_data):
+
+        """ Process the data from the form and set the scores """
+        """ question_list is 0 based, the form questions are 1-based """
+
+        # self.print_cleaned_data(cleaned_data)
+        questions = Questions()
+        questions_in_form = Questionnaire.get_question_count_for_slug(quiz_size_slug)
+        questions_answered = 0
+
+        for form_question_str in sorted(cleaned_data):
+            if not form_question_str.startswith("question_"):
+                continue
+            question_int = int(form_question_str.replace("question_", ""))
+            answer_123_type = questions.get_answer_123_type(question_int)
+            answer_str = cleaned_data[form_question_str]
+            if len(answer_str) > 0:
+                answer_int = int(answer_str)
+                answer_weight_str = questions.get_answer_weight(question_int, answer_str)
+                answer_weight_int = int(answer_weight_str)
+                self.tally_answer(answer_123_type, answer_int, answer_weight_int)
+                questions_answered += 1
+                if DJANGO_DEBUG:
+                    answer_text = questions.get_answer_text(question_int, answer_str)
+                    question_text = questions.get_question_text(question_int)
+                    print('Score.score_quiz -',
+                        str(question_int) + ' (' + answer_123_type + ')', '/',
+                        str(answer_int) + ' (' + answer_weight_str + ')',
+                        question_text, '/',
+                        answer_text)
+
+        print('Score - score_quiz: questions_answered/questions_in_form',
+                str(questions_answered) + '/' + str(questions_in_form))
+        self.unanswered_question_count = questions_in_form - questions_answered
+        if self.unanswered_question_count == 0:
+            self.score_is_complete = True
+        return self
+
+    def save_questionnaire(self, cleaned_data, quiz_size_slug):
+        email = cleaned_data["email"]
+        if email == '':
+            print( 'views.quiz: No email given, not saving quiz')
+        else:
+            print( 'views.quiz: saving quiz for "' + email + '"')
+            quiz_db = Questionnaire()
+            quiz_db.save_questionnaire(cleaned_data, quiz_size_slug)
+
+    def print_cleaned_data(self, cleaned_data):
+        """ print out the cleaned data, in order by question number """
+        print('Score.print_cleaned_data - cleaned_data:')
+
+        for question_xx in sorted(cleaned_data):
+            print('\tanswer for ' + question_xx + ': ' + cleaned_data[question_xx])
+
+    def tally_answer(self, answer_123_type, answer_int, answer_weight_int):
 
         """ Add the answer_weight to the appropriate score data member """
 
-        if answer_selected_int <= 3:
+        if answer_int <= 3:
             type_for_answer = answer_123_type
         else:
             type_for_answer = self.opposite_type[answer_123_type]
-
-        # print('Score.tally_answer - answer_123_type:', answer_123_type)
-        # print('Score.tally_answer - answer_selected_int:', answer_selected_int)
-        # print('Score.tally_answer - answer_weight_int:', answer_weight_int)
-        # print('Score.tally_answer - type_for_answer:', type_for_answer)
 
         if type_for_answer is "E":
             self.e_score += answer_weight_int
@@ -78,9 +128,31 @@ class Score(models.Model):
             self.p_score += answer_weight_int
 
         if DJANGO_DEBUG:
-            print('Score.tally_answer -',
-                    'adding', answer_weight_int, 'to', type_for_answer)
-            print('Score.tally_answer - self.__str__():',  self.__str__())
+            print('Score.tally_answer - added',
+                str(answer_weight_int) + ' to '+ type_for_answer + ': ',
+                self.__str__())
+        return True
+
+    def is_complete(self):
+        return self.score_is_complete
+
+    def set_incomplete_message(self, request):
+        if self.unanswered_question_count == 1:
+            incomplete_msg = 'There is ' + \
+                str(self.unanswered_question_count) + ' unanswered question'
+        else:
+            incomplete_msg = 'There are ' + \
+                str(self.unanswered_question_count) + ' unanswered questions'
+        messages.add_message(request, messages.ERROR, incomplete_msg)
+        return True
+
+    def set_quiz_results_messages(self, request):
+        """ Set the messages we display on the results page """
+        four_letter_type = "Type: " + self.as_four_letter_type()
+        pcts_and_counts_html = self.get_pcts_and_counts_html()
+        messages.add_message(request, messages.INFO, four_letter_type)
+        messages.add_message(request, messages.INFO, pcts_and_counts_html)
+        return True
 
     def as_four_letter_type(self):
         """ Return a string containing the four letter type """
@@ -116,16 +188,52 @@ class Score(models.Model):
 
         return four_letter_type
 
+    def get_pcts_and_counts_html(self):
+        """ Return an html string containing the score's percents and counts """
+        score_list = self.as_list_of_pcts_and_counts()
+        pcts_and_counts_html = '<ul>'
+        for score_pair in score_list:
+            pcts_and_counts_html += '<li>'
+            for single_score in score_pair:
+                pcts_and_counts_html += single_score + '&nbsp;'
+            pcts_and_counts_html += '</li>'
+        pcts_and_counts_html += '</ul>'
+        return pcts_and_counts_html
+
     def calculate_percentages(self):
         """ Calculate the percentages """
-        self.e_pct = round(100 * self.e_score / (self.e_score + self.i_score))
-        self.i_pct = round(100 * self.i_score / (self.e_score + self.i_score))
-        self.n_pct = round(100 * self.n_score / (self.n_score + self.s_score))
-        self.s_pct = round(100 * self.s_score / (self.n_score + self.s_score))
-        self.f_pct = round(100 * self.f_score / (self.f_score + self.t_score))
-        self.t_pct = round(100 * self.t_score / (self.f_score + self.t_score))
-        self.j_pct = round(100 * self.j_score / (self.j_score + self.p_score))
-        self.p_pct = round(100 * self.p_score / (self.j_score + self.p_score))
+        total_ei_score = self.e_score + self.i_score
+        total_ns_score = self.n_score + self.s_score
+        total_ft_score = self.f_score + self.t_score
+        total_jp_score = self.j_score + self.p_score
+
+        if total_ei_score > 0:
+            self.e_pct = round(100 * self.e_score / total_ei_score)
+            self.i_pct = round(100 * self.i_score / total_ei_score)
+        else:
+            self.e_pct = 0
+            self.i_pct = 0
+
+        if total_ns_score > 0:
+            self.n_pct = round(100 * self.n_score / total_ns_score)
+            self.s_pct = round(100 * self.s_score / total_ns_score)
+        else:
+            self.n_pct = 0
+            self.s_pct = 0
+
+        if total_ft_score > 0:
+            self.f_pct = round(100 * self.f_score / total_ft_score)
+            self.t_pct = round(100 * self.t_score / total_ft_score)
+        else:
+            self.f_pct = 0
+            self.t_pct = 0
+
+        if total_jp_score > 0:
+            self.j_pct = round(100 * self.j_score / total_jp_score)
+            self.p_pct = round(100 * self.p_score / total_jp_score)
+        else:
+            self.j_pct = 0
+            self.p_pct = 0
 
     def as_list_of_pcts_and_counts(self):
         """ Return a list containing both percentages and counts """
@@ -143,49 +251,6 @@ class Score(models.Model):
              'P: ' + str(self.p_pct) + '%&nbsp;(' + str(self.p_score) + ')']
         ]
         return score_list
-
-    def as_list_of_counts_and_pcts(self):
-        """ Return a list containing both counts and percentages """
-        if self.e_pct is None:
-            self.calculate_percentages()
-
-        score_list = [
-            ['E: ' + str(self.e_score) + '(' + str(self.e_pct) + '%)',
-             'I: ' + str(self.i_score) + '(' + str(self.i_pct) + '%)'],
-            ['N: ' + str(self.n_score) + '(' + str(self.n_pct) + '%)',
-             'S: ' + str(self.s_score) + '(' + str(self.s_pct) + '%)'],
-            ['F: ' + str(self.f_score) + '(' + str(self.f_pct) + '%)',
-             'T: ' + str(self.t_score) + '(' + str(self.t_pct) + '%)'],
-            ['J: ' + str(self.j_score) + '(' + str(self.j_pct) + '%)',
-             'P: ' + str(self.p_score) + '(' + str(self.p_pct) + '%)']
-        ]
-        return score_list
-
-    def as_percentages(self):
-        """ Return a string containing the percentages """
-        if self.e_pct is None:
-            self.calculate_percentages()
-
-        score_str  = 'E/I: ' + str(self.e_pct) + '%/' + str(self.i_pct) + '%; '
-        score_str += 'N/S: ' + str(self.n_pct) + '%/' + str(self.s_pct) + '%; '
-        score_str += 'F/T: ' + str(self.f_pct) + '%/' + str(self.t_pct) + '%; '
-        score_str += 'J/P: ' + str(self.j_pct) + '%/' + str(self.p_pct) + '%'
-        return score_str
-
-    def as_counts_and_pcts(self):
-        """ Return a string containing both counts and percentages """
-        if self.e_pct is None:
-            self.calculate_percentages()
-
-        score_str  = 'E: ' + str(self.e_score) + '(' + str(self.e_pct) + '%)/'
-        score_str += 'I: ' + str(self.i_score) + '(' + str(self.i_pct) + '%) - '
-        score_str += 'N: ' + str(self.n_score) + '(' + str(self.n_pct) + '%)/'
-        score_str += 'S: ' + str(self.s_score) + '(' + str(self.s_pct) + '%) - '
-        score_str += 'F: ' + str(self.f_score) + '(' + str(self.f_pct) + '%)/'
-        score_str += 'T: ' + str(self.t_score) + '(' + str(self.t_pct) + '%) - '
-        score_str += 'J: ' + str(self.j_score) + '(' + str(self.j_pct) + '%)/'
-        score_str += 'P: ' + str(self.p_score) + '(' + str(self.p_pct) + '%)'
-        return score_str
 
     def to_kv_pairs(self):
         """ Returns the current score as a list of key-value pairs """
@@ -216,28 +281,11 @@ class Score(models.Model):
         score_str += 'J/P: ' + str(self.j_score) + '/' + str(self.p_score)
         return score_str
 
-    #
-    # Crufty function(s) we wrote once but no longer use but may be useful
-    #
 
-    def to_list_of_lists(self):
-        """ Returns the current score as a list of lists """
-        score = [
-                ["E", self.e_score],
-                ["I", self.i_score],
-                ["N", self.n_score],
-                ["S", self.s_score],
-                ["F", self.f_score],
-                ["T", self.t_score],
-                ["J", self.j_score],
-                ["P", self.p_score],
-        ]
-        return score
+class Questions:
 
+    """ Read in and work with all the questions in the entire quiz """
 
-class Quiz(models.Model):
-
-    """ Model all the questions in the entire quiz """
 
     def __init__(self):
 
@@ -249,6 +297,10 @@ class Quiz(models.Model):
 
         """ Read the quiz questions and answers from the json file """
 
+        site_content_dir = os.path.abspath(os.path.dirname(__file__))
+        QUIZ_FILE_DIR = site_content_dir + '/static/content/json/quiz/'
+        QUIZ_FILE_NAME = 'seeourminds_quiz.json'
+
         quiz_file_path = QUIZ_FILE_DIR + QUIZ_FILE_NAME
         quiz_json_file = open(quiz_file_path)
         quiz_json_string = quiz_json_file.read()
@@ -256,32 +308,28 @@ class Quiz(models.Model):
         question_list = json.loads(quiz_json_string)
         return(question_list)
 
-    def get_quiz_question(self, list_question_int):
+    def get_quiz_question(self, question_int):
 
         """ Return the entire quiz question (answers, weights, etc.)"""
 
-        quiz_question = self.question_list[list_question_int]
-        # print('Quiz.get_quiz_question - list_question_int:', list_question_int)
-        # print('Quiz.get_quiz_question - quiz_question:', quiz_question)
+        quiz_question = self.question_list[question_int]
+        # print('Questions.get_quiz_question - question_int:', question_int)
+        # print('Questions.get_quiz_question - quiz_question:', quiz_question)
         return quiz_question
 
-    def get_label(self, list_question_int):
+    def get_question_text(self, question_int):
 
-        """ Get and return the question_text ("label") for the question """
-        """ list_question_int is 0 based, the question ids and labels are 1-based """
+        """ Get and return the question_text for the question """
 
-        quiz_question = self.get_quiz_question(list_question_int)
-        form_question_int = list_question_int + 1
-        label = str(form_question_int) + '. ' + quiz_question['question_text']
-        # print('Quiz.get_label - list_question_int:', list_question_int)
-        # print('Quiz.get_label - label:', label)
-        return label
+        quiz_question = self.get_quiz_question(question_int)
+        question_text = quiz_question['question_text']
+        return question_text
 
-    def get_choices(self, list_question_int):
+    def get_choices(self, question_int):
 
         """ Return the answer choices for the given question """
 
-        quiz_question = self.get_quiz_question(list_question_int)
+        quiz_question = self.get_quiz_question(question_int)
         choices = []
 
         if len(quiz_question['answer_1_text']) > 0 and \
@@ -321,78 +369,32 @@ class Quiz(models.Model):
             choice_7 = ['7', answer_7_text]
             choices.append(choice_7)
 
-        # print('Quiz.get_choices - list_question_int:', list_question_int)
-        # print('Quiz.get_choices - len(choices):', len(choices))
+        # print('Questions.get_choices - question_int:', question_int)
+        # print('Questions.get_choices - len(choices):', len(choices))
         return choices
 
-    def get_answer_123_type(self, list_question_int):
+    def get_answer_123_type(self, question_int):
 
         """ Get and return the answer_123_type (e.g., "E") for the question """
 
-        quiz_question = self.get_quiz_question(list_question_int)
+        quiz_question = self.get_quiz_question(question_int)
         answer_123_type = quiz_question['answer_123_type']
         return answer_123_type
 
-    def get_answer_text(self, list_question_int, answer_selected_str):
+    def get_answer_text(self, question_int, answer_str):
 
         """ Get and return the answer_X_text for the selected answer 'X' """
 
-        quiz_question = self.get_quiz_question(list_question_int)
-        answer_text_key = "answer_" + answer_selected_str + "_text"
+        quiz_question = self.get_quiz_question(question_int)
+        answer_text_key = "answer_" + answer_str + "_text"
         answer_text = quiz_question[answer_text_key]
         return answer_text
 
-    def get_answer_weight(self, list_question_int, answer_selected_str):
+    def get_answer_weight(self, question_int, answer_str):
 
         """ Get and return the answer_X_weight for the selected answer 'X' """
 
-        quiz_question = self.get_quiz_question(list_question_int)
-        answer_weight_key = "answer_" + answer_selected_str + "_weight"
+        quiz_question = self.get_quiz_question(question_int)
+        answer_weight_key = "answer_" + answer_str + "_weight"
         answer_weight = quiz_question[answer_weight_key]
         return answer_weight
-
-    def print_cleaned_data(self, cleaned_data):
-        """ print out the cleaned data, in order by question number """
-        print('Quiz.print_cleaned_data - cleaned_data:')
-
-        for question_xx in sorted(cleaned_data):
-            print('\tanswer for ' + question_xx + ': ' + cleaned_data[question_xx])
-
-    def score_quiz(self, cleaned_data):
-
-        """ Process the data from the form and set the scores """
-        """ question_list is 0 based, the form questions are 1-based """
-
-        # self.print_cleaned_data(cleaned_data)
-        score = Score()
-
-        for form_question_str in sorted(cleaned_data):
-            form_question_int = int(form_question_str.replace("question_", ""))
-            list_question_int = int(form_question_int) - 1
-            answer_123_type = self.get_answer_123_type(list_question_int)
-            answer_selected_str = cleaned_data[form_question_str]
-            answer_selected_int = int(answer_selected_str)
-            answer_text = self.get_answer_text(list_question_int, answer_selected_str)
-            answer_weight_str = self.get_answer_weight(list_question_int, answer_selected_str)
-            answer_weight_int = int(answer_weight_str)
-
-            # print('Quiz.score_quiz - form_question_str:',  str(form_question_str))
-            # print('Quiz.score_quiz - form_question_int:', str(form_question_int))
-            # print('Quiz.score_quiz - list_question_int:', str(list_question_int))
-            # print('Quiz.score_quiz - answer_123_type:',  answer_123_type)
-            # print('Quiz.score_quiz - answer_selected_str:',  answer_selected_str)
-            # print('Quiz.score_quiz - answer_selected_int:', answer_selected_int)
-            # print('Quiz.score_quiz - answer_text:',  answer_text)
-            # print('Quiz.score_quiz - answer_weight_str:',  answer_weight_str)
-            # print('Quiz.score_quiz - answer_weight_int:',  answer_weight_int)
-            # print('Quiz.score_quiz - score:',  score)
-
-            if DJANGO_DEBUG:
-                print('Quiz.score_quiz -',
-                        'question: ' + str(form_question_int) + ',',
-                        'type: ' + answer_123_type + ', ',
-                        'answer: ' + str(answer_selected_int))
-
-            score.tally_answer(answer_123_type, answer_selected_int, answer_weight_int)
-
-        return score
