591c308b38d6fa2ae5d10090a7425970a4d7f8e8
Tom W Hartung <tomwhartung@gmail.com>
2017-05-01 17:15:03 -0600

    Updated with the "new feature" of setting RUNNING_LOCALLY , and with new comments regarding what setting DJANGO_DEBUG does.

--- Site/bin/run.sh
+++ Site/bin/run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 #
 # run.sh: tiny shell script to run the development server for this app
+# --------------------------------------------------------------------
 #
 # Setting RUNNING_LOCALLY does the following:
-# - causes app to display grey boxes instead of ads
+# - causes site to display grey boxes instead of ads
 # - enables the tiny sized quiz (containing 4 questions for testing) option
+# - turns on the django DEBUG option (in settings.py)
+#   (this is NOT the same as DJANGO_DEBUG)
+#
+# Setting DJANGO_DEBUG does the following:
+# - prints detailed information about how quizzes are scored
 #
 export DJANGO_DEBUG=1
 export RUNNING_LOCALLY=1
