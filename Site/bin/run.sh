8cc950e1291f1db94a44115e958e759a4225fd9f
Tom W Hartung <tomwhartung@gmail.com>
2017-04-30 18:29:07 -0600

    Updated run.sh with comments describing what RUNNING_LOCALLY does, now that we have doubled its functionality.

--- Site/bin/run.sh
+++ Site/bin/run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #
-#  run.sh: tiny shell script to run the development server for this app
+# run.sh: tiny shell script to run the development server for this app
+#
+# Setting RUNNING_LOCALLY does the following:
+# - causes app to display grey boxes instead of ads
+# - enables the tiny sized quiz (containing 4 questions for testing) option
 #
 export DJANGO_DEBUG=1
 export RUNNING_LOCALLY=1
