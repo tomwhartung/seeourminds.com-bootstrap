4ecd9e991de1f570cabdea74f2fc7fdf2c59f4bc
Tom W Hartung <tomwhartung@gmail.com>
2017-06-13 13:56:32 -0600

    Changed "all_galleries_data" to "galleries_list_data" in views.py .

--- Site/content/views.py
+++ Site/content/views.py
@@ -63,10 +63,10 @@ def galleries(request):
     quiz_menu_data = Questionnaire.get_quiz_menu_data()
     title = 'All Galleries'
     galleries = Galleries()
-    all_galleries_data = galleries.get_galleries_list_data()
+    galleries_list_data = galleries.get_galleries_list_data()
     template = loader.get_template('content/galleries_list.html')
     context = {
-        'all_galleries_data': all_galleries_data,
+        'galleries_list_data': galleries_list_data,
         'adsense_ads': adsense_ads,
         'quiz_menu_data': quiz_menu_data,
         'title': title,
