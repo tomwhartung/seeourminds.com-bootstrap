000daa3e5284eadcbc4681aaf702136a3b4cda00
Tom W Hartung <tomwhartung@gmail.com>
2017-06-22 17:27:02 -0600

    Added assoc. arrays for the bootstrap_class_for_dom and bootstrap_class_for_aux .

--- Site/content/static/content/js/main.js
+++ Site/content/static/content/js/main.js
@@ -1,1 +1,50 @@
+/*
+ * Code for general use on seeourminds.com
+ */
+var seeourminds = {};
 
+/**
+ * Set the bootstrap class for the color corresponding to the
+ * dominant function for each four letter type
+ */
+seeourminds.bootstrap_class_for_dom = {
+  'ENFJ': 'btn-danger',
+  'ENFP': 'btn-primary',
+  'ENTJ': 'btn-success',
+  'ENTP': 'btn-primary',
+  'ESFJ': 'btn-danger',
+  'ESFP': 'btn-warning',
+  'ESTJ': 'btn-success',
+  'ESTP': 'btn-warning',
+  'INFJ': 'btn-primary',
+  'INFP': 'btn-danger',
+  'INTJ': 'btn-primary',
+  'INTP': 'btn-success',
+  'ISFJ': 'btn-warning',
+  'ISFP': 'btn-danger',
+  'ISTJ': 'btn-warning',
+  'ISTP': 'btn-success',
+}
+
+/**
+ * Set the bootstrap class for the color corresponding to the
+ * auxiliary function for each four letter type
+ */
+seeourminds.bootstrap_class_for_aux = {
+  'ENFJ': 'btn-primary',
+  'ENFP': 'btn-danger',
+  'ENTJ': 'btn-primary',
+  'ENTP': 'btn-success',
+  'ESFJ': 'btn-warning',
+  'ESFP': 'btn-danger',
+  'ESTJ': 'btn-warning',
+  'ESTP': 'btn-success',
+  'INFJ': 'btn-danger',
+  'INFP': 'btn-primary',
+  'INTJ': 'btn-success',
+  'INTP': 'btn-primary',
+  'ISFJ': 'btn-danger',
+  'ISFP': 'btn-warning',
+  'ISTJ': 'btn-success',
+  'ISTP': 'btn-warning',
+}
